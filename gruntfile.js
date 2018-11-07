065563ab477826866af2a3cd6f78f0578e7e0ec3
Bryon Vandiver <unicdk@gmail.com>
2018-11-06 16:55:43 -0800

    Checkpoint

--- gruntfile.js
+++ gruntfile.js
@@ -2,17 +2,6 @@ const fs = require("fs"),
       zlib = require("zlib"),
       os = require("os");
 
-function dest() {
-	switch (os.platform()) {
-	case 'darwin':
-		return "/Users/bryon/Library/Application Support/pico-8/carts/tiko.p8.png"
-	case 'win32':
-		return "C:\\Users\\unicd\\AppData\\Roaming\\pico-8\\carts\\tiko.p8.png"
-	default:
-		throw new Error(`Unknown platform ${os.platform()}`);
-	}
-}	
-
 function payload() {
 	var payload = new Array(0x4300);
 	for (var i = 0; i < 0x4300; i++) payload[i] = 0
@@ -25,14 +14,14 @@ module.exports = function(grunt) {
 		"pico": {
 			pico: {
 				src: ["runtime/main.lua", "runtime/**/*"],
-				dest: dest(),
+				dest: "tiko.p8.png",
 				payload: payload
 			}
 		},
 		"pico-run": {
 			pico: {
 				runtime: "/Applications/PICO-8.app/Contents/MacOS/pico8",
-				cartridge: dest()
+				cartridge: "tiko.p8.png"
 			}
 		},
 		watch: {
